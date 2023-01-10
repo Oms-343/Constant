f3b0b751e9699e408d0e3fc07a69f22e8a590d17
Om <oms87473@gmail.com>
2023-01-10 23:10:29 +0530

    fixing typo in route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pagess/SignUp";
 import Homepage from "./pagess/Homepage";
 import SignIn from "./pagess/SignIn";
 import Profile from "./pagess/Profile";
 import ForgetPassword from "./pagess/ForgetPassword";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -14,10 +14,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" exact element={<SignUp />} />
-          <Route path="home" element={<Homepage />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="forgotpassword" element={<ForgetPassword />} />
+
+          <Route path="/home" element={<Homepage />} />
+
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgotpassword" element={<ForgetPassword />} />
         </Routes>
       </Router>
 
