5e548f4ae3382d53545e4064192c6f82644e3451
Om <oms87473@gmail.com>
2023-01-18 23:54:02 +0530

    starting with the modal

--- src/componentss/MainContent.js
+++ src/componentss/MainContent.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const MainContent = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <main className=" flex justify-center items-center h-screen bg-yellow-300 text-2xl font-semibold text-white ">
-      <div className="flex flex-col">
+      <div className=" mb-14 flex flex-col pb-16 justify-center items-center">
         <h1>Add your first habit</h1>
-        <button className="bg-black mt-4">click me</button>
+        <IoMdAddCircleOutline
+          onClick={() => setShowModal(true)}
+          className="text-9xl cursor-pointer mt-3"
+        />
+        {showModal ? (
+          <Modal showModal={showModal} setShowModal={setShowModal} />
+        ) : null}
       </div>
     </main>
   );
