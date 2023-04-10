57cc5c83bdf160b9323c270486d8d94d25f6d954
Om <oms87473@gmail.com>
2023-04-10 23:26:38 +0530

    date component progress

--- src/componentss/MainContent.js
+++ src/componentss/MainContent.js
@@ -8,19 +8,24 @@ const MainContent = () => {
   const location = useLocation();
 
   return (
-    <main className=" flex justify-center items-center h-screen bg-yellow-300 text-2xl font-semibold text-white ">
-      <div className=" mb-14 flex flex-col pb-16 justify-center items-center">
-        <h1>Add your first habit</h1>
-
-        {page ? (
-          <Link to="/modal" state={{ background: location }}>
-            <IoMdAddCircleOutline className="text-9xl cursor-pointer mt-3" />
-          </Link>
-        ) : (
-          <h1>Date component</h1>
-        )}
-      </div>
-    </main>
+    <>
+      {page ? (
+        <main className=" flex justify-center items-center h-screen bg-yellow-300 text-2xl font-semibold text-white ">
+          <div className=" mb-14 flex flex-col pb-16 justify-center items-center">
+            <h1>Add your first habit</h1>
+            <Link to="/modal" state={{ background: location }}>
+              <IoMdAddCircleOutline className="text-9xl cursor-pointer mt-3" />
+            </Link>
+          </div>
+        </main>
+      ) : (
+        <main className=" flex justify-center items-center h-screen bg-yellow-300 text-2xl font-semibold text-white ">
+          <div className=" mb-14 flex flex-col pb-16 justify-center items-center w-full">
+            <h1>Date Component</h1>
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
