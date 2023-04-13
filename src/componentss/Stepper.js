aa5980e131009ce65ebd69abe95e68e765c7dbd5
Om <oms87473@gmail.com>
2023-04-13 23:33:48 +0530

    trying different approach for stepper

--- src/componentss/Stepper.js
+++ src/componentss/Stepper.js
@@ -1,40 +1,21 @@
-import React, { useState } from "react";
-import "./Stepper.css";
-import { TiTick } from "react-icons/ti";
+// import "./stepper.css";
+
 const Stepper = () => {
-  const steps = [1, 2, 3, 4];
-  const [currentStep, setCurrentStep] = useState(1);
-  const [complete, setComplete] = useState(false);
   return (
-    <>
-      <div className="flex justify-between">
-        {steps?.map((step, i) => (
-          <div
-            key={i}
-            className={`step-item ${currentStep === i + 1 && "active"} ${
-              (i + 1 < currentStep || complete) && "complete"
-            } `}
-          >
-            <div className="step">
-              {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
+    <div>
+      <div className="flex ">
+        {[1, 2, 3, 4].map(() => (
+          <div>
+            <div>
+              <span>1</span>
             </div>
-            <p className="text-gray-500">{step}</p>
+            <div>
+              <span>Address</span>
+            </div>
           </div>
         ))}
       </div>
-      {!complete && (
-        <button
-          className="btn"
-          onClick={() => {
-            currentStep === steps.length
-              ? setComplete(true)
-              : setCurrentStep((prev) => prev + 1);
-          }}
-        >
-          {currentStep === steps.length ? "Finish" : "Next"}
-        </button>
-      )}
-    </>
+    </div>
   );
 };
 
