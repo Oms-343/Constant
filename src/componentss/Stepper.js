823897fbf64d5290eb88e466c5af81a58c054e18
Om <oms87473@gmail.com>
2023-04-15 10:58:54 +0530

    removing classname package and using template literal instead

--- src/componentss/Stepper.js
+++ src/componentss/Stepper.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import cn from "classnames";
 
 const steps = [
   {
@@ -46,13 +45,12 @@ const Stepper = () => {
           className="absolute bg-purple-800 h-1 transition-all duration-400 ease-out top-1/2 transform -translate-y-1/2 left-0"
           style={{ width }}
         />
-        {steps.map(({ step, label }) => (
+        {steps.map(({ step }) => (
           <div key={step} className="relative">
             <div
-              className={cn(
-                "w-10 h-10 rounded-full bg-white border-3 border-solid flex justify-center items-center transition-all duration-400 ease-out",
+              className={`w-10 h-10 rounded-full bg-white border-3 border-solid flex justify-center items-center transition-all duration-400 ease-out ${
                 activeStep >= step ? "border-purple-800" : "border-pink-200"
-              )}
+              }`}
             >
               {activeStep > step ? (
                 <span className="text-purple-800 font-bold text-lg leading-none transform -scale-x-100 rotate-45">
@@ -64,28 +62,22 @@ const Stepper = () => {
                 </span>
               )}
             </div>
-            <div className="absolute top-10 left-1/2 transform -translate-x-1/2">
-              <span className="text-gray-800 text-sm">{label}</span>
-            </div>
           </div>
         ))}
       </div>
       <div className="flex justify-between mt-16">
         <button
-          className={cn(
-            "border border-solid rounded-md bg-gray-200 text-gray-800 font-medium py-2 px-6 transition-all duration-200 ease-out",
-            { "cursor-not-allowed opacity-50": activeStep === 1 }
-          )}
+          className={`border border-solid rounded-md bg-gray-200 text-gray-800 font-medium py-2 px-6 transition-all duration-200 ease-out ${
+            activeStep === 1 && "cursor-not-allowed opacity-50"
+          }`}
           onClick={prevStep}
           disabled={activeStep === 1}
         >
           Previous
         </button>
         <button
-          className={cn(
-            "border border-solid rounded-md bg-purple-800 text-white font-medium py-2 px-6 transition-all duration-200 ease-out",
-            { "cursor-not-allowed opacity-50": activeStep === totalSteps }
-          )}
+          className={`border border-solid rounded-md bg-purple-800 text-white font-medium py-2 px-6 transition-all duration-200 ease-out
+            ${activeStep === totalSteps && "cursor-not-allowed opacity-50"}`}
           onClick={nextStep}
           disabled={activeStep === totalSteps}
         >
