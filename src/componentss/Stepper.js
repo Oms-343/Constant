0fc50e36513282d864541f22dac5ed5372cad4d8
Om <oms87473@gmail.com>
2023-04-19 23:53:40 +0530

    removing extra style attribute

--- src/componentss/Stepper.js
+++ src/componentss/Stepper.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-
-const steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+import "./Stepper.css";
+const steps = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30,
+];
 
 const Stepper = () => {
   const [activeStep, setActiveStep] = useState(1);
@@ -18,18 +21,16 @@ const Stepper = () => {
   const width = `${(100 / (totalSteps - 1)) * (activeStep - 1)}%`;
 
   return (
-    <div className="w-full max-w-3xl mx-auto px-4">
+    <div className="w-full max-w-3xl mx-auto px-4 bg-red-500">
       <div
-        className="grid grid-cols-7 grid-rows-3 gap-4"
-        style={{
-          gridTemplateColumns: "repeat(7, 1fr)",
-          gridTemplateRows: "repeat(3, 1fr)",
-        }}
+        className="grid grid-cols-7 grid-rows-3 gap-4
+
+      "
       >
         {steps.map((step) => (
           <div key={step} className="relative">
             <div
-              className={`w-10 h-10 rounded-full bg-white border-3 border-solid flex justify-center items-center transition-all duration-400 ease-out ${
+              className={`    w-10 h-10 rounded-full bg-white border-3 border-solid flex justify-center items-center transition-all duration-400 ease-out ${
                 activeStep >= step ? "border-purple-800" : "border-pink-200"
               }`}
             >
@@ -46,6 +47,7 @@ const Stepper = () => {
           </div>
         ))}
       </div>
+
       <div className="flex justify-between mt-16">
         <button
           className={`border border-solid rounded-md bg-gray-200 text-gray-800 font-medium py-2 px-6 transition-all duration-200 ease-out ${
